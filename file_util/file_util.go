0fee162dea2903f7551c2145126e2c6f56b769db
menglingheng <menglingheng@soyoung.com>
2023-01-16 14:23:53 +0800

    init

--- file_util/file_util.go
+++ file_util/file_util.go
@@ -10,8 +10,11 @@ import (
 	"io/fs"
 	"net/http"
 	"os"
+	"os/user"
 	"path/filepath"
 	"regexp"
+	"runtime"
+	"strconv"
 	"strings"
 	"time"
 	stringsutil "utils/strings_util"
@@ -374,11 +377,6 @@ func UseMusl(path string) (bool, error) {
 	return false, nil
 }
 
-// IsReadable verify file readability
-func IsReadable(fileName string) (bool, error) {
-	return HasPermission(fileName, os.O_RDONLY)
-}
-
 // IsWriteable verify file writeability
 func IsWriteable(fileName string) (bool, error) {
 	return HasPermission(fileName, os.O_WRONLY)
@@ -396,4 +394,199 @@ func HasPermission(fileName string, permission int) (bool, error) {
 	file.Close()
 
 	return true, nil
+}
+
+// Basename 返回某个文件路径的文件名，suffix如果提供了则去掉改后缀
+func Basename(filename string, suffix ...string) string {
+	name := filepath.Base(filename)
+	for _, sf := range suffix {
+		name = strings.TrimRight(name, sf)
+	}
+	return name
+}
+
+// Chown 改变文件所属者
+func Chown(filename, username string) bool {
+	ex := FileExists(filename)
+	if !ex {
+		return false
+	}
+	// windows下无意义
+	if GetSystemOS() == "linux" {
+		sysUser, err := user.Lookup(username)
+		if err != nil {
+			return false
+		}
+		uid, err := strconv.Atoi(sysUser.Uid)
+		if err != nil {
+			return false
+		}
+		gid, err := strconv.Atoi(sysUser.Gid)
+		if err != nil {
+			return false
+		}
+		err = os.Chown(filename, uid, gid)
+		if err != nil {
+			return false
+		}
+	}
+	return true
+
+}
+
+// GetSystemOS 获取当前运行的操作系统类型
+func GetSystemOS() string {
+	return runtime.GOOS
+}
+
+// Copy 复制文件
+func Copy(src, dest string) bool {
+	if !FileExists(src) {
+		return false
+	}
+	fileSrc, err := os.Open(src)
+	if err != nil {
+		return false
+	}
+	defer fileSrc.Close()
+	fileDest, err := os.Create(dest)
+	if err != nil {
+		return false
+	}
+	defer fileDest.Close()
+	_, err = io.Copy(fileDest, fileSrc)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+// UnLink 删除文件
+func UnLink(filename string) bool {
+	return os.Remove(filename) == nil
+}
+
+// Dirname 返回路径中的目录部分
+func Dirname(path string) string {
+	return filepath.Dir(path)
+}
+
+// FileGetContents 获取文件内容
+func FileGetContents(filename string) []byte {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil
+	}
+	return data
+}
+
+// FilePutContents 写入文件内容
+func FilePutContents(filename string, content []byte, fileMode os.FileMode) (int, error) {
+	handle, err := os.OpenFile(filename, os.O_WRONLY, fileMode)
+	if err != nil {
+		return 0, err
+	}
+	defer handle.Close()
+	n, err := handle.Write(content)
+	if err != nil {
+		return n, err
+	}
+	return n, nil
+}
+
+// FileATime 获取文件上次访问时间
+// 对应windows下使用：
+// fileSys := sys.(*syscall.Win32FileAttributeData)
+// second := fileSys.LastAccessTime.Nanoseconds() / 1e9
+// 对于linux下使用
+// fileSys := sys.(*syscall.Stat_t)
+// second := fileSys.Atim.Sec
+func FileATime(filename string) (sys interface{}) {
+	info, _ := os.Stat(filename)
+	sys = info.Sys()
+	return
+}
+
+// FileMTime 获取文件修改时间
+func FileMTime(filename string) int64 {
+	info, _ := os.Stat(filename)
+	return info.ModTime().Unix()
+}
+
+// FileSize 获取文件大小
+func FileSize(filename string) int64 {
+	info, _ := os.Stat(filename)
+	return info.Size()
+}
+
+// IsDir 判断是否为文件夹
+func IsDir(filename string) bool {
+	info, err := os.Stat(filename)
+	if err != nil {
+		return false
+	}
+	return info.IsDir()
+}
+
+// IsFile 判断是否为文件
+func IsFile(filename string) bool {
+	info, err := os.Stat(filename)
+	if err != nil {
+		return false
+	}
+	return !info.IsDir()
+}
+
+// IsReadable 判断是否可读
+func IsReadable(filename string) bool {
+	h, err := os.Open(filename)
+	if err != nil {
+		return false
+	}
+	defer h.Close()
+	return true
+}
+
+// IsWritable 判断文件是否可写
+func IsWritable(filename string) bool {
+	h, err := os.OpenFile(filename, os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		return false
+	}
+	_ = h.Close()
+	return true
+}
+
+// MkDir 创建文件夹
+func MkDir(pathname string, fileMode os.FileMode, recursive bool) bool {
+	if recursive {
+		return os.MkdirAll(pathname, fileMode) == nil
+	}
+	return os.Mkdir(pathname, fileMode) == nil
+}
+
+// Rename 重命名
+func Rename(src, dest string) error {
+	return os.Rename(src, dest)
+}
+
+// ChMod 改变文件权限
+func ChMod(filename string, mode os.FileMode) bool {
+	return os.Chmod(filename, mode) == nil
+}
+
+// ChDir 改变当前目录
+func ChDir(dir string) bool {
+	return os.Chdir(dir) == nil
+}
+
+// GetCWD 取得当前工作目录
+func GetCWD() string {
+	dir, _ := os.Getwd()
+	return dir
+}
+
+// ReadDir 读取目录，golang里面不用opendir
+func ReadDir(dir string) ([]os.DirEntry, error) {
+	return os.ReadDir(dir)
 }
--- slice_util/slice_util.go
+++ slice_util/slice_util.go
@@ -1,8 +1,11 @@
 package slice_util
 
 import (
+	"fmt"
+	"golang.org/x/exp/constraints"
 	"math/rand"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -159,4 +162,200 @@ func Merge[V comparable](ss ...[]V) []V {
 // Merge and dedupe multiple items into a
 func MergeItems[V comparable](items ...V) []V {
 	return Dedupe(items)
+}
+
+// InArray PHP对应的in_array函数
+func InArray[T comparable](item T, arrayData []T) bool {
+	dataLen := len(arrayData)
+	if dataLen == 0 {
+		return false
+	}
+	for i := 0; i < dataLen; i++ {
+		if item == arrayData[i] {
+			return true
+		}
+	}
+	return false
+}
+
+// ArrayUnique 切片去重，目前仅支持int和string两种类型
+func ArrayUnique[T comparable](arrayData []T) []T {
+	dataLen := len(arrayData)
+	if dataLen == 0 {
+		return nil
+	}
+	results := make([]T, 0, dataLen)
+	m := make(map[T]struct{}, dataLen)
+	for i := 0; i < dataLen; i++ {
+		if _, ok := m[arrayData[i]]; !ok {
+			results = append(results, arrayData[i])
+			m[arrayData[i]] = struct{}{}
+		}
+	}
+	return results
+}
+
+// ArraySum 计算数组之和
+func ArraySum[T constraints.Float | constraints.Integer](data []T) T {
+	var total T
+	for _, num := range data {
+		total += num
+	}
+	return total
+}
+
+// ArrayChunk 整数版本的数组切割。将arrayData按照每个长度为length切割为子数组
+func ArrayChunk[T any](arrayData []T, length int) [][]T {
+	dataLen := len(arrayData)
+	if length == 0 {
+		return nil
+	}
+	var result [][]T
+	for i := 0; i < dataLen; i += length {
+		if length+i > dataLen {
+			result = append(result, arrayData[i:])
+		} else {
+			result = append(result, arrayData[i:i+length])
+		}
+	}
+	return result
+}
+
+// ArrayDiff 模拟PHP array_diff函数 计算差集
+func ArrayDiff[T comparable](base []T, othersParams ...[]T) []T {
+	if len(base) == 0 {
+		return nil
+	}
+	if len(base) > 0 && len(othersParams) == 0 {
+		return base
+	}
+	var tmp = make(map[T]int, len(base))
+	for _, v := range base {
+		tmp[v] = 1
+	}
+	for _, param := range othersParams {
+		for _, arg := range param {
+			if tmp[arg] != 0 {
+				tmp[arg]++
+			}
+		}
+	}
+	var res = make([]T, 0, len(tmp))
+	for k, v := range tmp {
+		if v == 1 {
+			res = append(res, k)
+		}
+	}
+	return res
+}
+
+// ArrayIntersect 模拟PHP array_intersect函数 计算交集
+func ArrayIntersect[T comparable](base []T, othersParams ...[]T) []T {
+	if len(base) == 0 {
+		return nil
+	}
+	if len(base) > 0 && len(othersParams) == 0 {
+		return base
+	}
+	var tmp = make(map[T]int, len(base))
+	for _, v := range base {
+		tmp[v] = 1
+	}
+	for _, param := range othersParams {
+		for _, arg := range param {
+			if tmp[arg] != 0 {
+				tmp[arg]++
+			}
+		}
+	}
+	min := len(othersParams) + 1
+	var res = make([]T, 0, len(tmp))
+	for k, v := range tmp {
+		if v >= min {
+			res = append(res, k)
+		}
+	}
+	return res
+}
+
+// ArrayMerge PHP array_merge函数，数组合并
+func ArrayMerge[T any](datas ...[]T) []T {
+	dataLen := len(datas)
+	if dataLen == 0 {
+		return nil
+	}
+	total := 0
+	for i := 0; i < dataLen; i++ {
+		total += len(datas[i])
+	}
+	results := make([]T, 0, total)
+	for i := 0; i < dataLen; i++ {
+		results = append(results, datas[i]...)
+	}
+	return results
+}
+
+// ArraySearch 搜索arrayData里面是否有item，有返回对应的index，无返回-1，只返回首次
+func ArraySearch[T comparable](item T, arrayData []T) int {
+	dataLen := len(arrayData)
+	if dataLen == 0 {
+		return -1
+	}
+	for i := 0; i < dataLen; i++ {
+		if arrayData[i] == item {
+			return i
+		}
+	}
+	return -1
+}
+
+// ArrayProduct 计算数组的各元素的乘积
+func ArrayProduct[T constraints.Float | constraints.Integer](data []T) T {
+	var result T = 1
+	for _, num := range data {
+		result *= num
+	}
+	return result
+}
+
+// ArrayWalk 遍历某一个数组切片，callback返回false则停止遍历
+func ArrayWalk[T any](data []T, callback func(item T, index int) bool) {
+	for i := 0; i < len(data); i++ {
+		if !callback(data[i], i) {
+			break
+		}
+	}
+}
+
+// Join 将一个int slice转为sep分割的字符串
+func Join[T constraints.Float | constraints.Integer | ~string](a []T, sep string) string {
+	var s strings.Builder
+	sLen := len(a)
+	for i, item := range a {
+		s.WriteString(fmt.Sprintf("%v", item))
+		if i < sLen-1 {
+			s.WriteString(sep)
+		}
+	}
+	return s.String()
+}
+
+// ArrayInt2ArrayString 将int slice转为string slice
+func ArrayInt2ArrayString(in []int) []string {
+	length := len(in)
+	data := make([]string, length)
+	for i := 0; i < length; i++ {
+		data[i] = strconv.Itoa(in[i])
+	}
+	return data
+}
+
+// ArrayReverse 数组反转，需要传指针进来，仅支持slice，array请转为slice
+func ArrayReverse[T any](array []T) {
+	if len(array) == 0 {
+		return
+	}
+	for i, j := 0, len(array)-1; i < j; i, j = i+1, j-1 {
+		array[i], array[j] = array[j], array[i]
+	}
 }
